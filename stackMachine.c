c8e11def76f7a74d89fd28c75f058dbc05a7eb6c
Owen Brahms <grizzlyThresher@gmail.com>
2020-01-26 02:15:14 -0500

    Converts prior Stack Machine into hybrid Stack/Register machine

--- stackMachine.c
+++ stackMachine.c
@@ -4,15 +4,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define TESTING
+#define DISPLAY
 
-int sp = MAX_DATASTACK_HEIGHT;
+int sp = -1;
 int bp = 0;
 int pc = 0;
 struct instruction* ir = 0;
-int gp = -1;
 int stack[MAX_DATASTACK_HEIGHT] = {};
 struct instruction code[MAX_CODE_LENGTH] = {};
+int registerFile[NUM_REGISTERS] = {};
 int halt = 1;
 int numLines = 0;
 
@@ -29,12 +29,12 @@ int main(int argc, char *argv[]) {
 		printf("Program too long for machine to run.");
 		return 0;
 	}
-    // Will only print the instructions and stack trace while testing the VM
+    // Will only print the instructions and stack trace while DISPLAY the VM
     // Avoids messiness for actual use of the machine.
-    #ifdef TESTING
+    #ifdef DISPLAY
 	printInstructions();
-	printf("\n\n                      gp    pc    bp    sp    data               stack\n");
-	printf("Initial values        %d    %d     %d     %d    \n\n",gp,pc,bp,sp);
+	printf("\n\n                          pc    bp    sp    registers\n");
+	printf("Initial values:           %d     %d     %d    0  0  0  0  0  0  0\n\n",pc,bp,sp);
     #endif
     int tmpPc = pc;
 
@@ -45,16 +45,16 @@ int main(int argc, char *argv[]) {
 		// Execute
 		switch (ir->op) {
 			case 1:
-				literal(ir->m);
+				literal(ir->r, ir->m);
                 break;
 			case 2:
-				operation(ir->m);
+				ret();
                 break;
 			case 3:
-				load(ir->l, ir->m);
+				load(ir->r, ir->l, ir->m);
                 break;
 			case 4:
-				store(ir->l, ir->m);
+				store(ir->r, ir->l, ir->m);
                 break;
 			case 5:
 				call(ir->l, ir->m);
@@ -66,10 +66,49 @@ int main(int argc, char *argv[]) {
 				jump(ir->m);
                 break;
 			case 8:
-				jmpIfZero(ir->m);
+				jmpIfZero(ir->r, ir->m);
                 break;
 			case 9:
-				sysOp(ir->m);
+				sysOp(ir->r, ir->m);
+                break;
+            case 10:
+                negate(ir->r, ir->m);
+                break;
+            case 11:
+                add(ir->r, ir->l, ir->m);
+                break;
+            case 12:
+                sub(ir->r, ir->l, ir->m);
+                break;
+            case 13:
+                multiply(ir->r, ir->l, ir->m);
+                break;
+            case 14:
+                divide(ir->r, ir->l, ir->m);
+                break;
+            case 15:
+                odd(ir->r, ir->m);
+                break;
+            case 16:
+                mod(ir->r, ir->l, ir->m);
+                break;
+            case 17:
+                equal(ir->r, ir->l, ir->m);
+                break;
+            case 18:
+                notEqual(ir->r, ir->l, ir->m);
+                break;
+            case 19:
+                less(ir->r, ir->l, ir->m);
+                break;
+            case 20:
+                lessOrEqual(ir->r, ir->l, ir->m);
+                break;
+            case 21:
+                greater(ir->r, ir->l, ir->m);
+                break;
+            case 22:
+                greaterOrEqual(ir->r, ir->l, ir->m);
                 break;
             default:
                 printf("Error: Invalid Operation.\n");
@@ -80,8 +119,8 @@ int main(int argc, char *argv[]) {
         // Increments the Program Counter
         pc++;
         
-        // Only prints the stack trace while testing the VM
-        #ifdef TESTING
+        // Only prints the stack trace while DISPLAY the VM
+        #ifdef DISPLAY
 		printState(tmpPc);
         #endif
 	}
@@ -90,124 +129,63 @@ int main(int argc, char *argv[]) {
 }
 
 // Methods for each ISA input
-void literal(int val) {
-    if (base(0,bp) == 0) {
-        gp++;
-        stack[gp] = val;
-    } else {
-        if (sp - 1 == gp) {
-            printf("Error: Stack Overflow.\n");
-            halt = 0;
-        }
-        sp--;
-        stack[sp] = val;
-    }
+void literal(int reg, int val) {
+    registerFile[reg] = val;
     
 }
-void operation(int op) {
-    switch (op) {
-        case 0:
-            ret();
-            break;
-        case 1:
-            negate();
-            break;
-        case 2:
-            add();
-            break;
-        case 3:
-            sub();
-            break;
-        case 4:
-            multiply();
-            break;
-        case 5:
-            divide();
-            break;
-        case 6:
-            odd();
-            break;
-        case 7:
-            mod();
-            break;
-        case 8:
-            equal();
-            break;
-        case 9:
-            notEqual();
-            break;
-        case 10:
-            less();
-            break;
-        case 11:
-            lessOrEqual();
-            break;
-        case 12:
-            greater();
-            break;
-        case 13:
-            greaterOrEqual();
-            break;
-        default:
-            printf("Error: Invalid Operation.\n");
-            halt = 0;
-            break;
-    }
-    
+void ret() {
+
 }
-void load(int lex, int offset) {
+void load(int reg, int lex, int offset) {
 
     
 }
-void store(int lex, int offset) {
+void store(int reg, int lex, int offset) {
 
     
 }
 void call(int lex, int loc) {
-    if (sp - 4 <= gp) {
+    if (sp + 4 >= MAX_DATASTACK_HEIGHT) {
         printf("Error: Stack Overflow.\n");
     } else {
-        stack[sp - 1] = 0;
-        stack[sp - 2] = base(lex, bp);
-        stack[sp - 3] = bp;
-        stack[sp - 4] = pc;
-        bp = sp - 1;
+        stack[sp + 1] = 0;
+        stack[sp + 2] = base(lex, bp);
+        stack[sp + 3] = bp;
+        stack[sp + 4] = pc;
+        bp = sp + 1;
         // Subtracts 1 to offset global pc increment
         pc = loc - 1;
     }
 
 }
 void inc(int numLocals) {
-    if (bp == 0) {
-        gp += numLocals;
+
+    if (sp + numLocals >= MAX_DATASTACK_HEIGHT) {
+        printf("Error: Stack Overflow.\n");
+        halt = 0;
     } else {
-        if (sp - numLocals <= gp) {
-            printf("Error: Stack Overflow.\n");
-            halt = 0;
-        } else {
-            sp -= numLocals;
-        }
+        sp += numLocals;
     }
-    
+        
 }
 void jump(int loc) {
     // Subtracts 1 to offset global pc increment
     pc = loc - 1;
 }
-void jmpIfZero(int loc) {
-    if(stack[sp] == 0) {
+void jmpIfZero(int reg, int loc) {
+    if(registerFile[reg] == 0) {
         // Subtracts 1 to offset global pc increment
         pc = loc - 1;
     }
     sp++;
 }
-void sysOp(int op) {
+void sysOp(int reg, int op) {
 	switch (op) {
 		case 1:
-			write();
+			write(reg);
             break;
 		case 2:
-			read();
+			read(reg);
             break;
 		case 3:
 			end();
@@ -219,59 +197,58 @@ void sysOp(int op) {
 	}
     
 }
-// Methods for each System Operation
-void write() {
+void negate(int stoReg, int froReg) {
 
 }
-void read() {
+void add(int stoReg, int reg1, int reg2) {
+
+}
+void sub(int stoReg, int reg1, int reg2) {
+
+}
+void multiply(int stoReg, int reg1, int reg2) {
+
+}
+void divide(int stoReg, int reg1, int reg2) {
+
+}
+void odd(int stoReg, int froReg) {
+
+}
+void mod(int stoReg, int reg1, int reg2) {
+
+}
+void equal(int stoReg, int reg1, int reg2) {
+
+}
+void notEqual(int stoReg, int reg1, int reg2) {
+
+}
+void less(int stoReg, int reg1, int reg2) {
+
+}
+void lessOrEqual(int stoReg, int reg1, int reg2) {
+
+}
+void greater(int stoReg, int reg1, int reg2) {
+
+}
+void greaterOrEqual(int stoReg, int reg1, int reg2) {
+
+}
+
+// Methods for each System Operation
+void write(int reg) {
+
+}
+void read(int reg) {
 
 }
 void end() {
 	halt = 0;
 }
-// Methods for each Op code for operation()
-void ret() {
 
-}
-void negate() {
 
-}
-void add() {
-
-}
-void sub() {
-
-}
-void multiply() {
-
-}
-void divide() {
-
-}
-void odd() {
-
-}
-void mod() {
-
-}
-void equal() {
-
-}
-void notEqual() {
-
-}
-void less() {
-
-}
-void lessOrEqual() {
-
-}
-void greater() {
-
-}
-void greaterOrEqual() {
-
-}
 // Method to separate the reading of the instructions from the rest of main
 int readInstructions(FILE* file) {
 
@@ -283,11 +260,13 @@ int readInstructions(FILE* file) {
 		if(numLines >= MAX_CODE_LENGTH) 
 			return 1;
 		
-		if(numCnt % 3 == 1) {
+		if(numCnt % 4 == 1) {
 			code[numLines].op = tmpNum;
-		} else if(numCnt % 3 == 2) {
-			code[numLines].l = tmpNum;
-		} else {
+		} else if(numCnt % 4 == 2) {
+			code[numLines].r = tmpNum;
+		} else if (numCnt % 4 == 3) {
+            code[numLines].l = tmpNum;
+        } else {
 			code[numLines].m = tmpNum;
 				numLines++;
 				numCnt=0;
@@ -297,62 +276,64 @@ int readInstructions(FILE* file) {
 	return 0;
 }
 
-#ifdef TESTING
+#ifdef DISPLAY
 char* oper[] = {"lit","opr","lod","sto","cal","inc","jmp","jpc","sio"};
 // Method to make printing of Instructions easier
 void printInstructions() {
 	//String array to translate from input number to corresponding operation 
-	printf("Line    Op    L    M \n");
+	printf("Line    Op    R   L    M \n");
+    char buffer[8] = {};
 	for(int i = 0; i < numLines; i++) {
-		printf("%d       %s   %d    %d \n", i, oper[code[i].op - 1], code[i].l, code[i].m);
+        makeBuffer(buffer, i, 7);
+		printf("%d%s%s    %d   %d    %d \n", i, buffer, oper[code[i].op - 1], code[i].r, code[i].l, code[i].m);
 	}
 
 }
 // Method used to print current state of the machine
 void printState(int curLoc) {
 
-    char buffer[6] = {};
+    printf("                          pc    bp    sp    registers\n");
+    char buffer[11] = {};
     // Prints out the current state with spacing determined by the number of digits
     // in the associated values.
-    makeBuffer(buffer, curLoc, 3);
+    makeBuffer(buffer, curLoc, 4);
     printf("%d%s", curLoc, buffer);
-    makeBuffer(buffer, gp, 6);
-    printf("%s  %d  %d          %d%s", oper[(ir->op)-1], ir->l, ir->m, gp, buffer);
+    makeBuffer(buffer, ir->m, 11);
+    printf("%s  %d  %d  %d%s%d", oper[(ir->op)-1], ir->r, ir->l, ir->m, buffer, pc);
     makeBuffer(buffer, pc, 6);
-    printf("%d%s", pc, buffer);
+    printf("%s%d", buffer, bp);
     makeBuffer(buffer, bp, 6);
-    printf("%d%s", bp, buffer);
+    printf("%s%d", buffer, sp);
     makeBuffer(buffer, sp, 6);
-    printf("%d%s", sp, buffer);
-
-    // Prints out the current state of the data-stack
-	for(int i = 0; i <= gp; i++) {
-		printf("%d", stack[i]);
-	}
-    if (sp < MAX_DATASTACK_HEIGHT) {
-
-        printf("|");
-
-        for(int i = gp+1; i < sp; i++) {
-            printf(" ");
-        }
-        for(int i = sp; i < MAX_DATASTACK_HEIGHT; i++) {
-            printf("%d", stack[i]);
-        }
+    printf("%s", buffer);
+    
+    // Prints out the current state of the register file
+    for (int i = 0; i < NUM_REGISTERS; i++) {
+        makeBuffer(buffer, registerFile[i], 3);
+        printf("%d%s", registerFile[i], buffer);
     }
-    printf("\n");
-
+    printf("\nStack: ");
+    // Prints out the current state of the data-stack
+	for (int i = 0; i <= sp; i++) {
+        printf("%d ", stack[i]);
+    }
+    printf("\n\n");
 }
 
 // Method used for formatting in the event a value requires 2 digits.
 // maxSize should always be <= max size of str.
 void makeBuffer(char *str, int num, int maxSize) {
 
-    int offset;
-    if (num / 10 > 0 || num < 0) {
-        offset = maxSize - 2;   
-    } else {
-        offset = maxSize - 1;
+    int offset = maxSize - 1;
+    
+    if (num / 10 > 0) {
+        offset--;
+        if (num / 100 > 0) {
+        offset--;
+        }
+    }
+    if (num < 0) {
+        offset--;
     }
 
     for(int i = 0; i < offset; i++) {
--- stackMachine.h
+++ stackMachine.h
@@ -1,42 +1,43 @@
 #include <stdio.h>
-#define MAX_DATASTACK_HEIGHT  23
-#define MAX_CODE_LENGTH  500
+#define MAX_DATASTACK_HEIGHT  40
+#define MAX_CODE_LENGTH  200
 #define MAX_LEXI_LEVELS  3
+#define NUM_REGISTERS 8
 // struct used to organize instructions
 struct instruction{
     int op;
+    int r;
     int l;
     int m;
 };
 // Methods for each ISA input
-void literal(int val);
-void operation(int op);
-void load(int lex, int offset);
-void store(int lex, int offset);
+void literal(int reg, int val);
+void ret();
+void load(int reg, int lex, int offset);
+void store(int reg, int lex, int offset);
 void call(int lex, int index);
 void inc(int numLocals);
 void jump(int loc);
-void jmpIfZero(int loc);
-void sysOp(int op);
+void jmpIfZero(int reg, int loc);
+void sysOp(int reg, int op);
+void negate(int stoReg, int froReg);
+void add(int stoReg, int reg1, int reg2);
+void sub(int stoReg, int reg1, int reg2);
+void multiply(int stoReg, int reg1, int reg2);
+void divide(int stoReg, int reg1, int reg2);
+void odd(int stoReg, int froReg);
+void mod(int stoReg, int reg1, int reg2);
+void equal(int stoReg, int reg1, int reg2);
+void notEqual(int stoReg, int reg1, int reg2);
+void less(int stoReg, int reg1, int reg2);
+void lessOrEqual(int stoReg, int reg1, int reg2);
+void greater(int stoReg, int reg1, int reg2);
+void greaterOrEqual(int stoReg, int reg1, int reg2);
 // Methods for each System Operation
-void write();
-void read();
+void write(int reg);
+void read(int reg);
 void end();
-// Methods for each Op code for operation()
-void ret();
-void negate();
-void add();
-void sub();
-void multiply();
-void divide();
-void odd();
-void mod();
-void equal();
-void notEqual();
-void less();
-void lessOrEqual();
-void greater();
-void greaterOrEqual();
+
 // Method to separate the reading of the instructions from the rest of main
 int readInstructions(FILE* file);
 // Method to make printing of Instructions easier
