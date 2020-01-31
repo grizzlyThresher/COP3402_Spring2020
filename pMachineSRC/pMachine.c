52c077eb6548894d49984e8afa7367eeb1b97cd9
Owen Brahms <grizzlyThresher@gmail.com>
2020-01-31 00:18:30 -0500

    Fixes CALs ability to jump to instruction. Updates test.

--- pMachineSRC/pMachine.c
+++ pMachineSRC/pMachine.c
@@ -148,6 +148,7 @@ int main(int argc, char *argv[]) {
                     stack[sp + 4] = pc;
                     bp = sp + 1;
 
+                    pc = ir.m;
                     curLex++;
                 }
                 break;
