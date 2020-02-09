127dbb9ab52039ce2f313f01e997388006f470e5
Owen Brahms <grizzlyThresher@gmail.com>
2020-02-09 16:33:28 -0500

    Adds initial Lexer file

---
+++ Lexer/lexer.c
@@ -0,0 +1,12 @@
+// Owen Brahms - 3739927
+// Michaela Green - 4198343
+// COP 3402, Spring 2020
+// Project 2
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+	
+}
