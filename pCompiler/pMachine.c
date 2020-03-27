a866c1fd869c436cdbcc1326e69e1e9be3f929f6
Michaela Green <mcgreen417@knights.ucf.edu>
2020-03-27 01:12:35 -0400

    Execute function changed to void and prints instructions; header file updates

--- pCompiler/pMachine.c
+++ pCompiler/pMachine.c
@@ -8,17 +8,17 @@
 #include "pCompiler.h"
 
 
-int execute(instruction* code, FILE* opr) {   
+void execute(instruction* code, FILE* opr, int printMachine, int printParse, int numInstructions) {   
 
     // Instantiation of Global variables
     int sp = 0;
     int bp = 1;
     int pc = 0;
     instruction ir;
+    char buffer[8] = {};
     int* stack = calloc(MAX_DATASTACK_HEIGHT, sizeof(int));
     int* registerFile = calloc(NUM_REGISTERS, sizeof(int));
     int halt = 0;
-    int numLines = 0;
 
     // Keeps track of legicographical level for printing purposes
     int curLex = 0;
@@ -28,13 +28,32 @@ int execute(instruction* code, FILE* opr) {
     "neg", "add", "sub", "mul", "div", "odd", "mod", "eql", "neq",
     "lss", "leq", "gtr", "geq"};
 
+    // Prints instructions to output file
+    for(int i = 0; i < numInstructions; i++) {
+        makeBuffer(buffer, i, 7);
+        fprintf(opr, "%d%s%s    %d   %d    %d \n", i, buffer,
+            operations[(code[i].op) - 1], code[i].r, code[i].l, code[i].m);
+    }
+
+    // Prints instructions to console IF printParse is active
+    if (printParse == 1) {
+        for(int i = 0; i < numInstructions; i++) {
+            makeBuffer(buffer, i, 7);
+            fprintf(stdout, "%d%s%s    %d   %d    %d \n", i, buffer,
+                operations[(code[i].op) - 1], code[i].r, code[i].l, code[i].m);
+        }
+    }
+
 	fprintf(opr, "\n\n                          pc    bp    sp    registers\n");
+    if(printMachine == 1) fprintf(stdout, "\n\n                          pc    bp    sp    registers\n");
 	fprintf(opr, "Initial values:           %d     %d     %d  ", pc,bp,sp);
+    if(printMachine == 1) fprintf(stdout, "Initial values:           %d     %d     %d  ", pc,bp,sp);
     for (int i = 0; i < NUM_REGISTERS; i++) {
         fprintf(opr, "   0");
+        if(printMachine == 1) fprintf(stdout, "   0");
     }
     fprintf(opr, "\n\n");
-
+    if(printMachine == 1) fprintf(stdout, "\n\n");
 
     // Temp variable used to store pc value before execution
     int tmpPc = pc;
@@ -171,22 +190,20 @@ int execute(instruction* code, FILE* opr) {
                 break;
             default:
                 BAD_OPERATION
-                return 0;
+                halt = 1;
                 break;
 		}
         // Prints out current state of the machine
-		printState(stack, tmpPc, ir, pc, bp, sp, registerFile, curLex, opr, operations);
+		printState(stack, tmpPc, ir, pc, bp, sp, registerFile, curLex, opr, operations, printMachine);
 	}
 
     // Frees all remaining pointers on the heap
     free(stack);
     free(registerFile);
-    
-    return 0;
 }
 
 // Method used to print current state of the machine
-void printState(int* stack, int curLoc, instruction ir, int pc, int bp, int sp, int* regFile, int lex, FILE* output, char** oper) {
+void printState(int* stack, int curLoc, instruction ir, int pc, int bp, int sp, int* regFile, int lex, FILE* output, char** oper, int printMachine) {
 
 //    fprintf(opr, "                          pc    bp    sp    registers\n");
     char *buffer = calloc(11, sizeof(char));
@@ -194,36 +211,47 @@ void printState(int* stack, int curLoc, instruction ir, int pc, int bp, int sp, 
     // in the associated values.
     makeBuffer(buffer, curLoc, 4);
     fprintf(output, "%d%s", curLoc, buffer);
+    if(printMachine == 1) fprintf(stdout, "%d%s", curLoc, buffer);
     makeBuffer(buffer, ir.m, 11);
     fprintf(output, "%s  %d  %d  %d%s%d", oper[(ir.op)-1], ir.r, ir.l, ir.m, buffer, pc);
+    if(printMachine == 1) fprintf(stdout, "%s  %d  %d  %d%s%d", oper[(ir.op)-1], ir.r, ir.l, ir.m, buffer, pc);
     makeBuffer(buffer, pc, 6);
     fprintf(output, "%s%d", buffer, bp);
+    if(printMachine == 1) fprintf(stdout, "%s%d", buffer, bp);
     makeBuffer(buffer, bp, 6);
     fprintf(output, "%s%d", buffer, sp);
+    if(printMachine == 1) fprintf(stdout, "%s%d", buffer, sp);
     makeBuffer(buffer, sp, 6);
     fprintf(output, "%s", buffer);
+    if(printMachine == 1) fprintf(stdout, "%s", buffer);
     
     // Prints out the current state of the register file
     for (int i = 0; i < NUM_REGISTERS; i++) {
         makeBuffer(buffer, regFile[i], 4);
         fprintf(output, "%d%s", regFile[i], buffer);
+        if(printMachine == 1) fprintf(stdout, "%d%s", regFile[i], buffer);
     }
 
     // Prints out the current state of the stack
     fprintf(output, "\nStack: ");
+    if(printMachine == 1) fprintf(stdout, "\nStack: ");
     int lineCnt = lex;
 
 	for (int i = 1; i <= sp; i++) {
         if (lineCnt > 0 && i == base(stack, lineCnt - 1, bp)) {
             fprintf(output, "| %d ", stack[i]);
+            if(printMachine == 1) fprintf(stdout, "| %d ", stack[i]);
             lineCnt--;
         } else {
             fprintf(output, "%d ", stack[i]);
+            if(printMachine == 1) fprintf(output, "%d ", stack[i]);
         }
     }
     fprintf(output, "\n\n");
+    if(printMachine == 1) fprintf(output, "\n\n");
     free(buffer);
 }
+
 // Method used for formatting in the event a value requires 2 digits.
 // maxSize should always be <= max size of str.
 void makeBuffer(char *str, int num, int maxSize) {
